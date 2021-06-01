3a3253b5fe857daeb1a5454c1513603bdcb9079a
dinitrogen <noshowdon@gmail.com>
2021-06-01 15:27:53 -0400

    Added rainbow and first attempt at grayscale.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('#container');
 const resizeGrid = document.querySelector('#resizeGrid');
-
+const randColorButton = document.querySelector('#randColorButton');
+const grayscaleButton = document.querySelector('#grayscaleButton');
+let randRed, randGreen, randBlue, grayShade = 255; 
 
 // initialize grid size
 let gridSize = 16;
@@ -12,12 +14,16 @@ drawGrid();
 // resize grid when button is clicked
 resizeGrid.addEventListener('click', changeGrid);
 
+// change palette to rainbow when button is clicked
+randColorButton.addEventListener('click', changePaletteRand);
 
+// change palette to grayscale when button is clicked
+grayscaleButton.addEventListener('click', changePaletteGrayscale);
 
 function drawGrid() {
     // clear current grid
     while (container.firstChild) {
-        container.removeChild(container.lastChild);
+        container.removeChild(container.lastChild); // 
     }
     // draw new grid
     for (i = 0; i < gridArea; i++) {
@@ -26,8 +32,48 @@ function drawGrid() {
         container.appendChild(gridPixel);
         gridPixel.addEventListener('mouseenter', function() {
             gridPixel.classList.add('filledPixel');
-        })
+        });
     }
+}
+
+// change color palette to random
+function changePaletteRand () {
+    // clear current grid
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    };
+    // draw new grid
+    for (i = 0; i < gridArea; i++) {
+        let gridPixel = document.createElement('div');
+        gridPixel.classList='gridPixel';
+        container.appendChild(gridPixel);
+        gridPixel.addEventListener('mouseenter', function toRainbow() {
+            randColor();
+            gridPixel.style.backgroundColor = "rgb(" + randRed + "," + randGreen + "," + randBlue + ")";
+            gridPixel.removeEventListener('mouseenter', toRainbow);
+        });  
+    };   
+}
+
+// change color palette to grayscale
+function changePaletteGrayscale () {
+    // clear current grid
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    };
+    // draw new grid
+    for (i = 0; i < gridArea; i++) {
+        let gridPixel = document.createElement('div');
+        gridPixel.classList='gridPixel';
+        container.appendChild(gridPixel);
+        gridPixel.addEventListener('mouseenter', function toGrayscale() {
+            grayShade -= 26;
+            if (grayShade < 0) {
+                grayShade = 0;
+            }
+            gridPixel.style.backgroundColor = "rgb(" + grayShade + "," + grayShade + "," + grayShade + ")";
+        });  
+    };   
 }
 
 
@@ -40,7 +86,14 @@ function changeGrid() {
     drawGrid();
 }
 
+
+
 // color randomizer
 function randColor() {
-    
+    randRed = Math.round(Math.random() * 255);
+    randGreen = Math.round(Math.random() * 255);
+    randBlue = Math.round(Math.random() * 255);
+    document.documentElement.style.setProperty("--randRed", randRed);
+    document.documentElement.style.setProperty("--randGreen", randGreen);
+    document.documentElement.style.setProperty("--randBlue", randBlue);
 }
